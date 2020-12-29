d61ca3dc0fbef483dc2f073379e1eb817560f176
Rotem Meron <rm13rotem@gmail.com>
2020-12-29 14:20:39 +0200

    VisitGroup can be either visits or other visitGroups

--- MainStaticMaintainableEntities/VisitGroup/VisitGroup.cs
+++ MainStaticMaintainableEntities/VisitGroup/VisitGroup.cs
@@ -1,13 +1,72 @@
-﻿using MainStaticMaintainableEntities.VisitAssembly;
+﻿using MainStaticMaintainableEntities.Providers;
+using MainStaticMaintainableEntities.VisitAssembly;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using Utilities;
 
 namespace MainStaticMaintainableEntities.VisitGroup
 {
     public class VisitGroup : PersistantEntity
     {
-        public List<VisitGroup> VisitGroups { get; set; }
-        public List<Visit> Visits { get; set; }
+        private List<VisitGroup> _visitGroups;
+        private List<Visit> _visits;
+
+        public List<VisitGroup> VisitGroups
+        {
+            get { return _visitGroups; }
+            set
+            {
+                if (value != null)
+                    _visits = null;
+                _visitGroups = value;
+            }
+        }
+
+        public List<Visit> Visits
+        {
+            get { return _visits; }
+            set
+            {
+                if (value != null)
+                    _visitGroups = null;
+                _visits = value;
+            }
+        }
+
+        public PersistantEntity ToPersistantEntity()
+        {
+            JsonValue = JsonFormatter.ToJson<VisitGroup>(this);
+            return this;
+        }
+        public void LoadFromPersistantEntity()
+        {
+            var loaded = JsonFormatter.FromJson<VisitGroup>(this.JsonValue);
+            if (loaded.VisitGroups != null) this.VisitGroups = loaded.VisitGroups;
+            else if (loaded.Visits != null) this.Visits = loaded.Visits;
+        }
+
+        public bool AddVisitGroupById(int visitGroupId)
+        {
+            IRepositoryLocator<VisitGroup> visitGroupLocator = new RepositoryLocator<VisitGroup>();
+            IRepository<VisitGroup> repository = visitGroupLocator.GetRepository(RepositoryType.FromJsonRepository, null);
+            VisitGroup toBeAdded = repository.GetByExpression(x => x.Id == visitGroupId).FirstOrDefault();
+            if (VisitGroups == null)
+                VisitGroups = new List<VisitGroup>();
+            VisitGroups.Add(toBeAdded);
+            return true;
+        }
+
+        public bool AddVisitById(int visitId)
+        {
+            IRepositoryLocator<Visit> visitLocator = new RepositoryLocator<Visit>();
+            IRepository<Visit> repository = visitLocator.GetRepository(RepositoryType.FromJsonRepository, null);
+            Visit toBeAdded = repository.GetByExpression(x => x.Id == visitId).FirstOrDefault();
+            if (Visits == null)
+                Visits = new List<Visit>();
+            Visits.Add(toBeAdded);
+            return true;
+        }
     }
 }
