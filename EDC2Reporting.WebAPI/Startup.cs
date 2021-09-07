ed2cfcb6606c062fe4492d2da2754ad82d83a2f3
Rotem Meron <rm13rotem@gmail.com>
2021-09-07 21:57:39 +0300

    Removing the start of a specific controller for each and every separate PersistantEntity (like Site, Doctor, module, visit, patient and so on)

--- EDC2Reporting.WebAPI/Startup.cs
+++ EDC2Reporting.WebAPI/Startup.cs
@@ -4,10 +4,14 @@ using MainStaticMaintainableEntities.Providers;
 using MainStaticMaintainableEntities.SiteAssembly;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using JavaScriptEngineSwitcher.V8;
+using JavaScriptEngineSwitcher.Extensions.MsDependencyInjection;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using React.AspNet;
 
 namespace EDC2Reporting.WebAPI
 {
@@ -23,7 +27,13 @@ namespace EDC2Reporting.WebAPI
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
+            services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+            services.AddReact();
+            // Make sure a JS engine is registered, or you will get an error!
+            services.AddJsEngineSwitcher(options => options.DefaultEngineName = V8JsEngine.EngineName)
+              .AddV8();
+
+            services.AddControllersWithViews();
 
             if (Configuration.GetValue<string>("Environment") == "DEV")
             {
@@ -51,7 +61,7 @@ namespace EDC2Reporting.WebAPI
                     options.UseSqlServer(Configuration.GetConnectionString("DevConnection")));
             }
 
-
+            
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -61,6 +71,28 @@ namespace EDC2Reporting.WebAPI
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            // Initialise ReactJS.NET. Must be before static files.
+            app.UseReact(config =>
+            {
+                // If you want to use server-side rendering of React components,
+                // add all the necessary JavaScript files here. This includes
+                // your components as well as all of their dependencies.
+                // See http://reactjs.net/ for more information. Example:
+                //config
+                //  .AddScript("~/js/First.jsx")
+                //  .AddScript("~/js/Second.jsx");
+
+                // If you use an external build too (for example, Babel, Webpack,
+                // Browserify or Gulp), you can improve performance by disabling
+                // ReactJS.NET's version of Babel and loading the pre-transpiled
+                // scripts. Example:
+                //config
+                //  .SetLoadBabel(false)
+                //  .AddScriptWithoutTransform("~/js/bundle.server.js");
+            });
+
+            app.UseStaticFiles();
 
             app.UseHttpsRedirection();
 
