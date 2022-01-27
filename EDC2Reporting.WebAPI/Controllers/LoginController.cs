321f014ae448e0bf1d1a76b4e25147c5c0b11790
Rotem Meron <rm13rotem@gmail.com>
2022-01-27 16:51:26 +0200

    v1.0.0.18 - Adding LoginController and RegisterController. Persistent Entities are common to all trails running on the machine.

---
+++ EDC2Reporting.WebAPI/Controllers/LoginController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDC2Reporting.WebAPI.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult FirstLogin()
+        {
+            // hardest - the URL with Id, GuidId, existingRole, DefaultPassword 1234
+            // assigns user the quicklookId
+            return View();
+        }
+
+        public IActionResult Login() 
+        {
+            // usual - quicklookId, username, and password
+            // validates details - BirthDate, DoctorNumber, etc.
+            // generates quickpassword
+
+
+            return View();
+        }
+
+        public IActionResult QuickLogin()
+        {
+            // just the quicklookId, the yearOfBirth and a quickpassword
+            return View();
+        }
+    }
+}
---
+++ EDC2Reporting.WebAPI/Controllers/RegisterController.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EDC2Reporting.WebAPI.Controllers
+{
+    public class RegisterController : Controller
+    {
+        public IActionResult InviteAdministrator()
+        {
+            // If not administrator - goto home/error
+            // notify by alert email all existing administrators
+
+            // Send Mail
+            return View();
+        }
+
+        public IActionResult InviteClinicalTrialLeader()
+        {
+            // If not administartor or CTL - go to home error
+            // notify by alert email all existing CTLs
+
+            // Send Mail
+            return View();
+        }
+
+        public IActionResult InviteSiteManager()
+        {
+            // If not admin or CTL or Site Manager - got to home error
+
+            // Send Mail
+            return View();
+        }
+        public IActionResult InviteUser()
+        {
+
+            // Send Mail
+            return View();
+        }
+        public IActionResult InviteAdministrator()
+        {
+            // notify by alert email all existing administrators
+            return View();
+        }
+    }
+}
